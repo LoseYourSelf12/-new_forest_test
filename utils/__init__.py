071982777a7bcabc6ec98dbaa11844d16b79dd1e
Andrey <loseyourself1290@gmail.com>
2025-06-04 15:57:25 +0300

    Add dataset utilities and complete configs

---
+++ utils/__init__.py
@@ -0,0 +1,9 @@
+from .config import name_id_dict
+from .nameiddict_cor import nameiddict_cor
+from .datasets import (
+    Dataset,
+    DATASET_PATHS,
+    load_diff_files,
+    use_diff_file,
+    combine,
+)
